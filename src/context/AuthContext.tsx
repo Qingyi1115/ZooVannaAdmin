8a34ad5eb92964106e08a8497b7a746c989b88b3
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2023-09-08 01:42:55 +0800

    Fix returning to homepage on refresh or direct link

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -55,7 +55,7 @@ type AuthContextProps = {
 // AuthContextProps is for TypeScript to verify the type
 function AuthContextProvider({ children }: AuthContextProps): JSX.Element {
   const [state, dispatch] = useReducer(authReducer, {
-    user: null,
+    user: JSON.parse(localStorage.getItem("user") || "null"),
   });
 
   useEffect(() => {
@@ -63,6 +63,8 @@ function AuthContextProvider({ children }: AuthContextProps): JSX.Element {
 
     if (user) {
       dispatch({ type: "LOGIN", payload: user });
+    } else {
+      dispatch({ type: "LOGOUT" });
     }
   }, []);
 
