85d937f6c4c183abd2e0844f4804011600303010
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2023-09-21 03:09:12 +0800

    Fixed all employees datatable not showing

--- src/components/EmployeeAccountManagement/AllEmployeesDatatable.tsx
+++ src/components/EmployeeAccountManagement/AllEmployeesDatatable.tsx
@@ -9,70 +9,74 @@ import { NavLink } from "react-router-dom";
 import { HiCheck, HiPencil, HiTrash, HiX } from "react-icons/hi";
 import { Button } from "@/components/ui/button";
 
-{/*const toast = useRef<Toast>(null);*/}
+{
+  /*const toast = useRef<Toast>(null);*/
+}
 
 function AllEmployeesDatatable() {
-    const apiJson = useApiJson();
+  const apiJson = useApiJson();
 
-    let employee: Employee = {
-      employeeId: -1,
-      employeeName: "",
-      employeeEmail: "",
-      employeeAddress: "",
-      employeePhoneNumber: "",
-      employeePasswordhash: "",
-      employeeSalt: "",
-      employeeDoorAccessCode: "",
-      employeeEducation: "",
-      employeeBirthDate: new Date(),
-      isAccountManager: false,
-      dateOfResignation: new Date(),
-      employeeProfileUrl: ""
+  let employee: Employee = {
+    employeeId: -1,
+    employeeName: "",
+    employeeEmail: "",
+    employeeAddress: "",
+    employeePhoneNumber: "",
+    employeePasswordhash: "",
+    employeeSalt: "",
+    employeeDoorAccessCode: "",
+    employeeEducation: "",
+    employeeBirthDate: new Date(),
+    isAccountManager: false,
+    dateOfResignation: new Date(),
+    employeeProfileUrl: "",
+  };
+
+  const toast = useRef<Toast>(null);
+
+  const [employeeList, setEmployeeList] = useState<Employee[]>([]);
+  const [selectedEmployee, setSelectedEmployee] = useState<Employee>(employee);
+  const dt = useRef<DataTable<Employee[]>>(null);
+  const [globalFilter, setGlobalFilter] = useState<string>("");
+  const request = { includes: [] };
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const responseJson = await apiJson.post(
+          "http://localhost:3000/api/employee/getAllEmployees",
+          { includes: "" }
+        );
+        setEmployeeList(responseJson.employees as Employee[]);
+        // console.log("Here " + responseJson);
+        // const help = responseJson as Employee[];
+        // console.log(help);
+      } catch (error: any) {
+        console.log(error);
+      }
     };
+    fetchEmployees();
+  }, []);
 
-    const toast = useRef<Toast>(null);
-      
-    const [employeeList, setEmployeeList] = useState<Employee[]>([]);
-    const [selectedEmployee, setSelectedEmployee] = useState<Employee>(employee);
-    const dt = useRef<DataTable<Employee[]>>(null);
-    const [globalFilter, setGlobalFilter] = useState<string>("");
-    const request = {includes: []};
-    
-    useEffect(() => {
-      const fetchEmployees = async () => {
-        try {
-          const responseJson = await apiJson.get(
-            "http://localhost:3000/api/employee/getAllEmployees"
-          );
-          setEmployeeList(responseJson as Employee[])
-          console.log("Here " + responseJson);
-          const help = responseJson as Employee[];
-          console.log(help);
-        } catch (error: any) {
-          console.log(error);
-        }
-      };
-      fetchEmployees();
-    }, []);
+  const header = (
+    <div className="flex flex-wrap items-center justify-between gap-2">
+      <h4 className="m-1">Manage Employees</h4>
+      <span className="p-input-icon-left">
+        <i className="pi pi-search" />
+        <InputText
+          type="search"
+          placeholder="Search..."
+          onInput={(e) => {
+            const target = e.target as HTMLInputElement;
+            setGlobalFilter(target.value);
+          }}
+        />
+      </span>
+    </div>
+  );
 
-    const header = (
-        <div className="flex flex-wrap items-center justify-between gap-2">
-            <h4 className="m-1">Manage Employees</h4>
-            <span className="p-input-icon-left">
-            <i className="pi pi-search" />
-            <InputText
-                type="search"
-                placeholder="Search..."
-                onInput={(e) => {
-                const target = e.target as HTMLInputElement;
-                setGlobalFilter(target.value);
-                }}
-            />
-            </span>
-        </div>
-    );
-
-    {/*const actionBodyTemplate = (employee: Employee) => {
+  {
+    /*const actionBodyTemplate = (employee: Employee) => {
         return (
           <React.Fragment>
             <NavLink to={`/facility/editfacility/${employee.employeeName}`}>
@@ -91,79 +95,80 @@ function AllEmployeesDatatable() {
             </Button>
           </React.Fragment>
         );
-      };*/}
+      };*/
+  }
 
-    return (
+  return (
+    <div>
+      <div>
+        <Toast ref={toast} />
         <div>
-            <div>
-                <Toast ref={toast} />
-                <div>
-                    <DataTable
-                        ref={dt}
-                        value={employeeList}
-                        selection={selectedEmployee}
-                        onSelectionChange={(e) => {
-                        if (Array.isArray(e.value)) {
-                            setSelectedEmployee(e.value);
-                        }
-                        }}
-                        dataKey="employeeId"
-                        paginator
-                        rows={10}
-                        selectionMode={"single"}
-                        rowsPerPageOptions={[5, 10, 25]}
-                        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-                        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} employees"
-                        globalFilter={globalFilter}
-                        header={header}
-                    >
-                        <Column
-                        field="employeeName"
-                        header="Name"
-                        sortable
-                        style={{ minWidth: "12rem" }}
-                        ></Column>
-                        <Column
-                        field="employeeEmail"
-                        header="Email"
-                        sortable
-                        style={{ minWidth: "12rem" }}
-                        ></Column>
-                        <Column
-                        field="employeeAddress"
-                        header="Employee Address"
-                        sortable
-                        style={{ minWidth: "12rem" }}
-                        ></Column>
-                        <Column
-                        field="employeePhoneNumber"
-                        header="Phone Number"
-                        sortable
-                        style={{ minWidth: "12rem" }}
-                        ></Column>
-                        <Column
-                        field="employeeEducation"
-                        header="Education"
-                        sortable
-                        style={{ minWidth: "12rem" }}
-                        ></Column>
-                        <Column
-                        field="employeeBirthDate"
-                        header="Birthday"
-                        sortable
-                        style={{ minWidth: "12rem" }}
-                        ></Column>
-                        {/*<Column
+          <DataTable
+            ref={dt}
+            value={employeeList}
+            selection={selectedEmployee}
+            onSelectionChange={(e) => {
+              if (Array.isArray(e.value)) {
+                setSelectedEmployee(e.value);
+              }
+            }}
+            dataKey="employeeId"
+            paginator
+            rows={10}
+            selectionMode={"single"}
+            rowsPerPageOptions={[5, 10, 25]}
+            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords} employees"
+            globalFilter={globalFilter}
+            header={header}
+          >
+            <Column
+              field="employeeName"
+              header="Name"
+              sortable
+              style={{ minWidth: "12rem" }}
+            ></Column>
+            <Column
+              field="employeeEmail"
+              header="Email"
+              sortable
+              style={{ minWidth: "12rem" }}
+            ></Column>
+            <Column
+              field="employeeAddress"
+              header="Employee Address"
+              sortable
+              style={{ minWidth: "12rem" }}
+            ></Column>
+            <Column
+              field="employeePhoneNumber"
+              header="Phone Number"
+              sortable
+              style={{ minWidth: "12rem" }}
+            ></Column>
+            <Column
+              field="employeeEducation"
+              header="Education"
+              sortable
+              style={{ minWidth: "12rem" }}
+            ></Column>
+            <Column
+              field="employeeBirthDate"
+              header="Birthday"
+              sortable
+              style={{ minWidth: "12rem" }}
+            ></Column>
+            {/*<Column
                         body={actionBodyTemplate}
                         header="Actions"
                         exportable={false}
                         style={{ minWidth: "18rem" }}
                     ></Column>*/}
-                    </DataTable>
-                </div>
-            </div>
+          </DataTable>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default AllEmployeesDatatable;
