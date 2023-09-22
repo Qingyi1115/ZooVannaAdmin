93b5edf357c8c2fdd0558c814c3d65005e3e2efe
Owner <e0540022@u.nus.edu>
2023-09-23 03:26:33 +0800

    Update Profile.tsx

--- src/components/EmployeeCommonInfra/Profile.tsx
+++ src/components/EmployeeCommonInfra/Profile.tsx
@@ -5,10 +5,11 @@ import useApiJson from "../../hooks/useApiJson";
 import Employee from "src/models/Employee";
 import { InputText } from "primereact/inputtext";
 import { Column } from "primereact/column";
-import { NavLink } from "react-router-dom";
+import * as Form from "@radix-ui/react-form";
 import { HiCheck, HiPencil, HiTrash, HiX } from "react-icons/hi";
 import { Button } from "@/components/ui/button";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { NavLink } from "react-router-dom";
 
 {
   /*const toast = useRef<Toast>(null);*/
@@ -59,32 +60,96 @@ function Profile() {
 
   return (
     <div>
-      <div>
+      
+    <Form.Root
+      className="flex w-full flex-col gap-6 rounded-lg border border-stroke bg-white p-20 text-black shadow-default dark:border-strokedark"
+      onSubmit={()=>{}}
+      encType="multipart/form-data"
+    >
         <div>
         <h1> Welcome {curEmployee.employeeName}!</h1>
         </div>
         <br />
-        <div>
-        <h1> Email : {curEmployee.employeeEmail}!</h1>
-        </div>
-        <br />
-        <div>
-        <h1> Address : {curEmployee.employeeAddress}!</h1>
-        </div>
-        <br />
-        <div>
-        <h1> Access Code : {curEmployee.employeeDoorAccessCode}!</h1>
-        </div>
-        <br />
-        <div>
-        <h1> Phone Number : {curEmployee.employeePhoneNumber}!</h1>
-        </div>
-        <br />
-        <div>
-        <h1> Education : {curEmployee.employeeEducation}!</h1>
-        </div>
-        <br />
+
+        <Form.Field
+      name=""
+      className="flex w-full flex-col gap-1 data-[invalid]:text-danger"
+    >
+        <Form.Control
+        type="text"
+        required={true}
+        placeholder={`Email : ${curEmployee.employeeEmail}`}
+        value={`Email : ${curEmployee.employeeEmail}`}
+        step={0.5}
+        pattern=""
+        onChange={(e) => {}}
+        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium shadow outline-none transition hover:bg-whiten focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+        />
+        </Form.Field>
         
+        <Form.Field
+      name=""
+      className="flex w-full flex-col gap-1 data-[invalid]:text-danger"
+    >
+        <Form.Control
+        type="text"
+        required={true}
+        placeholder={`Address : ${curEmployee.employeeAddress}`}
+        value={`Address : ${curEmployee.employeeAddress}`}
+        step={0.5}
+        pattern=""
+        onChange={(e) => {}}
+        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium shadow outline-none transition hover:bg-whiten focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+        />
+        </Form.Field>
+        
+        <Form.Field
+      name=""
+      className="flex w-full flex-col gap-1 data-[invalid]:text-danger"
+    >
+        <Form.Control
+        type="text"
+        required={true}
+        placeholder={`Access Code : ${curEmployee.employeeDoorAccessCode}`}
+        value={`Access Code : ${curEmployee.employeeDoorAccessCode}`}
+        step={0.5}
+        pattern=""
+        onChange={(e) => {}}
+        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium shadow outline-none transition hover:bg-whiten focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+        />
+        </Form.Field>
+        
+        <Form.Field
+      name=""
+      className="flex w-full flex-col gap-1 data-[invalid]:text-danger"
+    >
+        <Form.Control
+        type="text"
+        required={true}
+        placeholder={`Phone Number : ${curEmployee.employeePhoneNumber}`}
+        value={`Phone Number : ${curEmployee.employeePhoneNumber}`}
+        step={0.5}
+        pattern=""
+        onChange={(e) => {}}
+        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium shadow outline-none transition hover:bg-whiten focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+        />
+        </Form.Field>
+        
+        <Form.Field
+      name=""
+      className="flex w-full flex-col gap-1 data-[invalid]:text-danger"
+    >
+        <Form.Control
+        type="text"
+        required={true}
+        placeholder={`Education : ${curEmployee.employeeEducation}`}
+        value={`Education : ${curEmployee.employeeEducation}`}
+        step={0.5}
+        pattern=""
+        onChange={(e) => {}}
+        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium shadow outline-none transition hover:bg-whiten focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+        />
+        </Form.Field>
       <React.Fragment>
         <NavLink to={"/updateProfile"}>
           <Button className="mr-2">
@@ -93,7 +158,8 @@ function Profile() {
           </Button>
         </NavLink>
       </React.Fragment>
-      </div>
+      
+    </Form.Root>
     </div>
   );
 }
