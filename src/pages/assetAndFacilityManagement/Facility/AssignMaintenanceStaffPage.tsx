f5af7c88e7957fb9e2cee0f8535cd51dfd2348cd
Thian Jiawen <thianjiawen@u.nus.edu>
2023-09-24 01:13:29 +0800

    Renamed AssignGeneralStaff to ManageMaintenanceStaff

--- src/pages/assetAndFacilityManagement/Facility/AssignMaintenanceStaffPage.tsx
+++ src/pages/assetAndFacilityManagement/Facility/AssignMaintenanceStaffPage.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useApiJson from "../../../hooks/useApiJson";
 import Employee from "src/models/Employee";
-import AssignMaintenanceStaff from "../../../components/AssetAndFacilityManagement/FacilityManagement/AssignGeneralStaff";
+import AssignMaintenanceStaff from "../../../components/AssetAndFacilityManagement/FacilityManagement/ManageMaintenanceStaff";
 
 
 
