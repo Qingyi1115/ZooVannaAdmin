14885e9017333a5e286d817fa485245c028a8884
Thian Jiawen <thianjiawen@u.nus.edu>
2023-09-23 17:13:55 +0800

    Fixed UI for view facility details

--- src/pages/assetAndFacilityManagement/Facility/ViewFacilityDetailsPage.tsx
+++ src/pages/assetAndFacilityManagement/Facility/ViewFacilityDetailsPage.tsx
@@ -22,53 +22,61 @@ import ViewInHouseDetails from "../../../components/AssetAndFacilityManagement/F
 
 
 function ViewFacilityDetailsPage() {
-    const apiJson = useApiJson();
-    let facility: Facility = {
-      facilityId: -1,
-      facilityName: "",
-      xCoordinate: 0,
-      yCoordinate: 0,
-      facilityDetail: "",
-      facilityDetailJson: undefined
+  const apiJson = useApiJson();
+  let facility: Facility = {
+    facilityId: -1,
+    facilityName: "",
+    xCoordinate: 0,
+    yCoordinate: 0,
+    facilityDetail: "",
+    facilityDetailJson: undefined,
+    isSheltered: false
+  };
+
+  const { facilityId } = useParams<{ facilityId: string }>();
+  const [curFacility, setCurFacility] = useState<Facility>(facility);
+  const [refreshSeed, setRefreshSeed] = useState<number>(0);
+
+  const curThirdParty = curFacility.facilityDetail == "thirdParty" ? curFacility.facilityDetailJson : undefined;
+  const curInHouse = curFacility.facilityDetail == "inHouse" ? curFacility.facilityDetailJson : undefined;
+
+  useEffect(() => {
+    const fetchFacilities = async () => {
+      try {
+        const responseJson = await apiJson.post(
+          `http://localhost:3000/api/assetFacility/getFacility/${facilityId}`,
+          { includes: [] }
+        );
+        console.log(responseJson);
+        setCurFacility(responseJson.facility as Facility);
+      } catch (error: any) {
+        console.log(error);
+      }
     };
 
-    const { facilityId } = useParams<{ facilityId: string}>();
-    const [curFacility, setCurFacility] = useState<Facility>(facility);
-    const [refreshSeed, setRefreshSeed] = useState<number>(0);
+    fetchFacilities();
+  }, [refreshSeed]);
 
-    const curThirdParty = curFacility.facilityDetail=="thirdParty" ? curFacility.facilityDetailJson : undefined;
-    const curInHouse = curFacility.facilityDetail=="inHouse" ? curFacility.facilityDetailJson : undefined;
+  return (
+    <div className="p-10">
+      <div className="flex w-full flex-col gap-6 rounded-lg border border-stroke bg-white p-20 text-black shadow-lg">
+        <div className="relative flex flex-col">
+          <ViewFacilityDetails curFacility={curFacility}></ViewFacilityDetails>
+          {curThirdParty && <ViewThirdPartyDetails curThirdParty={curThirdParty}></ViewThirdPartyDetails>}
+          {curInHouse && <ViewInHouseDetails curInHouse={curInHouse}></ViewInHouseDetails>}
+        </div>
 
-    useEffect(() => {
-        const fetchFacilities = async () => {
-          try {
-            const responseJson = await apiJson.post(
-              `http://localhost:3000/api/assetFacility/getFacility/${facilityId}`,
-              {includes:[]}
-            );
-            console.log(responseJson);
-            setCurFacility(responseJson.facility as Facility);
-          } catch (error: any) {
-            console.log(error);
-          }
-        };
-    
-        fetchFacilities();
-    }, [refreshSeed]);
-
-      return (
-        <div className="my-4 flex justify-start gap-6">
-        <NavLink to={`/assetfacility/editfacility/${facilityId}/assignstaff`}>
-          <Button>Assign Maintenance Staff</Button>
-        </NavLink>
+        <div className="mb-4 flex justify-start gap-6 self-center">
+          <NavLink to={`/assetfacility/editfacility/${facilityId}/assignstaff`}>
+            <Button>Assign Maintenance Staff</Button>
+          </NavLink>
           <NavLink to={`/assetfacility/editfacility/${facilityId}/removestaff`}>
             <Button>Remove Maintenance Staff</Button>
           </NavLink>
-        <ViewFacilityDetails curFacility={curFacility}></ViewFacilityDetails>
-        {curThirdParty && <ViewThirdPartyDetails curThirdParty={curThirdParty}></ViewThirdPartyDetails> }
-        {curInHouse && <ViewInHouseDetails curInHouse={curInHouse}></ViewInHouseDetails> }
         </div>
-      );
-    }
+      </div>
+    </div>
+  );
+}
 
 export default ViewFacilityDetailsPage;
