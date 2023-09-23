7863959bf4f1f73f6fc329667e1b2afa247066b2
Thian Jiawen <thianjiawen@u.nus.edu>
2023-09-24 04:42:38 +0800

    Create CreateNewFacilityLogPage.tsx

---
+++ src/pages/assetAndFacilityManagement/Facility/CreateNewFacilityLogPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateNewFacilityLogForm from "../../../components/AssetAndFacilityManagement/FacilityManagement/CreateNewFacilityLogForm";
+
+function CreateNewFacilityLogPage() {
+  return (
+    <div className="p-10">
+      <CreateNewFacilityLogForm />
+    </div>
+  );
+}
+
+export default CreateNewFacilityLogPage;
