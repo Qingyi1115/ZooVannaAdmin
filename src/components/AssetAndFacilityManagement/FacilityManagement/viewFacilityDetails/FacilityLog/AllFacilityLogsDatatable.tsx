d52f62614feccdfca7e443fe24dd5c535b841947
Thian Jiawen <thianjiawen@u.nus.edu>
2023-10-07 20:54:33 +0800

    Fixed default sort order for logs

--- src/components/AssetAndFacilityManagement/FacilityManagement/viewFacilityDetails/FacilityLog/AllFacilityLogsDatatable.tsx
+++ src/components/AssetAndFacilityManagement/FacilityManagement/viewFacilityDetails/FacilityLog/AllFacilityLogsDatatable.tsx
@@ -176,7 +176,7 @@ function AllFacilityLogsDatatable(props: AllFacilityLogsDatatableProps) {
   }
   const [sortKey, setSortKey] = useState<string>('');
   const [sortOrder, setSortOrder] = useState<1 | 0 | -1 | undefined | null>(0);
-  const [sortField, setSortField] = useState<string>('');
+  const [sortField, setSortField] = useState<string>('!dateTime');
   const sortOptions: SortOption[] = [
     { label: 'Latest log', value: '!dateTime' },
     { label: 'Earliest log', value: 'dateTime' },
@@ -281,7 +281,7 @@ function AllFacilityLogsDatatable(props: AllFacilityLogsDatatableProps) {
       <div>
         <Toast ref={toast} />
         <div className="">
-         
+
           <DataView
             value={facilityLogList}
             itemTemplate={itemTemplate}
