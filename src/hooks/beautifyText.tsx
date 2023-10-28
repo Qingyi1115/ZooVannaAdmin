0f6ad707f94bc94c259f6fb7479f4b307404a265
Owner <e0540022@u.nus.edu>
2023-10-29 00:27:07 +0800

    Update beautifyText.tsx

--- src/hooks/beautifyText.tsx
+++ src/hooks/beautifyText.tsx
@@ -1,5 +1,5 @@
 function beautifyText(ugly: string | undefined | null) {
-    if (ugly == null || ugly == undefined) {
+    if (ugly === null || ugly === undefined || ugly.length == 0) {
         return "";
     } else {
         return ugly.split("_").map(word => word[0].toLocaleUpperCase() + word.substring(1, word.length).toLocaleLowerCase()).join(" ");
