4ed5cabb0eb8a86a253e86e38d3736f838b872fb
Thian Jiawen <thianjiawen@u.nus.edu>
2023-11-02 04:47:32 +0800

    Might regret this

--- src/components/EventManagement/CreateZooEventPage/CreatePublicZooEventForm.tsx
+++ src/components/EventManagement/CreateZooEventPage/CreatePublicZooEventForm.tsx
@@ -6,15 +6,86 @@ import FormFieldSelect from "../../FormFieldSelect";
 import useApiJson from "../../../hooks/useApiJson";
 import { useToast } from "@/components/ui/use-toast";
 import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Separator } from "@/components/ui/separator";
 import {
+  AcquisitionMethod,
+  AnimalGrowthStage,
+  AnimalSex,
   EventType,
 } from "../../../enums/Enumurated";
 import { Calendar } from "primereact/calendar";
 
 import { Nullable } from "primereact/ts-helpers";
 import beautifyText from "../../../hooks/beautifyText";
+import { InputText } from "primereact/inputtext";
+import { Dialog } from "primereact/dialog";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import Animal from "../../../models/Animal";
+import EnrichmentItem from "../../../models/EnrichmentItem";
+import Species from "../../../models/Species";
+import { useAuthContext } from "../../../hooks/useAuthContext";
+import { HiCheck, HiTrash, HiX } from "react-icons/hi";
+import AllEmployeesDatatable from "../../EmployeeAccountManagement/AllEmployeesDatatable";
+
+let emptySpecies: Species = {
+  speciesId: -1,
+  speciesCode: "",
+  commonName: "",
+  scientificName: "",
+  aliasName: "",
+  conservationStatus: "",
+  domain: "",
+  kingdom: "",
+  phylum: "",
+  speciesClass: "",
+  order: "",
+  family: "",
+  genus: "",
+  nativeContinent: "",
+  nativeBiomes: "",
+  educationalDescription: "",
+  educationalFunFact: "",
+  groupSexualDynamic: "",
+  habitatOrExhibit: "habitat",
+  imageUrl: "",
+  generalDietPreference: "",
+  ageToJuvenile: 0,
+  ageToAdolescent: 1,
+  ageToAdult: 2,
+  ageToElder: 3,
+  lifeExpectancyYears: 0,
+};
+let emptyAnimal: Animal = {
+  animalId: -1,
+  animalCode: "",
+  imageUrl: "",
+  isGroup: false,
+  houseName: "",
+  identifierType: "",
+  identifierValue: "",
+  sex: AnimalSex.MALE,
+  dateOfBirth: new Date(),
+  placeOfBirth: "",
+  acquisitionMethod: AcquisitionMethod.INHOUSE_CAPTIVE_BRED,
+  dateOfAcquisition: new Date(),
+  acquisitionRemarks: "",
+  physicalDefiningCharacteristics: "",
+  behavioralDefiningCharacteristics: "",
+  dateOfDeath: null,
+  locationOfDeath: null,
+  causeOfDeath: null,
+  growthStage: AnimalGrowthStage.ADOLESCENT,
+  animalStatus: "",
+  species: emptySpecies,
+};
+
+let emptyItem: EnrichmentItem = {
+  enrichmentItemId: -1,
+  enrichmentItemImageUrl: "",
+  enrichmentItemName: "",
+};
 
 function CreatePublicZooEventForm() {
   const apiFormData = useApiFormData();
@@ -30,6 +101,16 @@ function CreatePublicZooEventForm() {
   const [eventDescription, setEventDescription] = useState<string>("");
   const [eventDates, setEventDates] = useState<Date[]>([]);
   const [eventNotificationDate, setEventNotificationDate] = useState<Nullable<Date>>(null);
+
+  const [involvedAnimalList, setInvolvedAnimalList] = useState<Animal[]>();
+  const [involvedAnimalGlobalFiler, setInvolvedAnimalGlobalFilter] =
+    useState<string>("");
+  const employee = useAuthContext().state.user?.employeeData;
+  const [involvedItemList, setInvolvedItemList] = useState<EnrichmentItem[]>(
+    []
+  );
+  const [involvedItemGlobalFiler, setInvolvedItemGlobalFilter] =
+    useState<string>("");
 
   // validate functions
   function validateIdentifierType(props: ValidityState) {
@@ -183,6 +264,215 @@ function CreatePublicZooEventForm() {
     };
     createPublicZooEventApi();
   }
+
+  const animalImageBodyTemplate = (rowData: Animal) => {
+    return (
+      <img
+        src={"http://localhost:3000/" + rowData.imageUrl}
+        alt={rowData.houseName}
+        className="aspect-square w-16 rounded-full border border-white object-cover shadow-4"
+      />
+    );
+  };
+
+  const enrichmentItemImageBodyTemplate = (rowData: EnrichmentItem) => {
+    return (
+      <img
+        src={"http://localhost:3000/" + rowData.enrichmentItemImageUrl}
+        alt={rowData.enrichmentItemName}
+        className="aspect-square w-16 rounded-full border border-white object-cover shadow-4"
+      />
+    );
+  };
+
+  /////
+  // remove animal stuff
+  const [selectedAnimal, setSelectedAnimal] = useState<Animal>(emptyAnimal);
+  const [removeAnimalDialog, setRemoveAnimalDialog] = useState<boolean>(false);
+  const confirmRemoveAnimal = (animal: Animal) => {
+    setSelectedAnimal(animal);
+    setRemoveAnimalDialog(true);
+  };
+
+  const hideRemoveAnimalDialog = () => {
+    setRemoveAnimalDialog(false);
+  };
+
+  const removeAnimal = async () => {
+    if (involvedAnimalList == undefined) {
+      toastShadcn({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "Involved animal list is invalid",
+      });
+      return;
+    }
+
+    let _animals = involvedAnimalList.filter(
+      (val) => val.animalId !== selectedAnimal?.animalId
+    );
+
+    let animalCode = selectedAnimal.animalCode;
+    const animalToRemoveApiObj = {
+      animalActivityId,
+      animalCode,
+    };
+
+    const removeAnimalApi = async () => {
+      try {
+        const responseJson = await apiJson.put(
+          "http://localhost:3000/api/animal/removeAnimalFromActivity/",
+          animalToRemoveApiObj
+        );
+
+        toastShadcn({
+          // variant: "destructive",
+          title: "Deletion Successful",
+          description:
+            "Successfully removed animal: " +
+            selectedAnimal.houseName +
+            " from the activity",
+        });
+        setInvolvedAnimalList(_animals);
+        setRemoveAnimalDialog(false);
+        setSelectedAnimal(emptyAnimal);
+      } catch (error: any) {
+        // got error
+        toastShadcn({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description:
+            "An error has occurred while removing animal from activity: \n" +
+            error.message,
+        });
+      }
+    };
+    removeAnimalApi();
+  };
+
+  const removeAnimalDialogFooter = (
+    <React.Fragment>
+      <Button onClick={hideRemoveAnimalDialog}>
+        <HiX className="mr-2" />
+        No
+      </Button>
+      <Button variant={"destructive"} onClick={removeAnimal}>
+        <HiCheck className="mr-2" />
+        Yes
+      </Button>
+    </React.Fragment>
+  );
+
+  ////
+  const animalActionBodyTemplate = (animal: Animal) => {
+    return (
+      <React.Fragment>
+        <div className="mx-auto">
+          <Button
+            variant={"destructive"}
+            className="mr-2"
+            onClick={() => confirmRemoveAnimal(animal)}
+          >
+            <HiTrash className="mx-auto" />
+          </Button>
+        </div>
+      </React.Fragment>
+    );
+  };
+
+  /////
+  // remove item stuff
+  const [selectedItem, setSelectedItem] = useState<EnrichmentItem>(emptyItem);
+  const [removeItemDialog, setRemoveItemDialog] = useState<boolean>(false);
+  const confirmRemoveItem = (item: EnrichmentItem) => {
+    setSelectedItem(item);
+    setRemoveItemDialog(true);
+  };
+
+  const hideRemoveItemDialog = () => {
+    setRemoveItemDialog(false);
+  };
+
+  const removeItem = async () => {
+    if (involvedItemList == undefined) {
+      toastShadcn({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "Involved animal list is invalid",
+      });
+      return;
+    }
+
+    let _animals = involvedItemList.filter(
+      (val) => val.enrichmentItemId !== selectedItem?.enrichmentItemId
+    );
+
+    let enrichmentItemId = selectedItem.enrichmentItemId;
+    const animalToRemoveApiObj = {
+      animalActivityId,
+      enrichmentItemId,
+    };
+
+    const removeItemApi = async () => {
+      try {
+        const responseJson = await apiJson.put(
+          "http://localhost:3000/api/animal/removeItemFromActivity/",
+          animalToRemoveApiObj
+        );
+
+        toastShadcn({
+          // variant: "destructive",
+          title: "Deletion Successful",
+          description:
+            "Successfully removed animal: " +
+            selectedItem.enrichmentItemName +
+            " from the activity",
+        });
+        setInvolvedItemList(_animals);
+        setRemoveItemDialog(false);
+        setSelectedItem(emptyItem);
+      } catch (error: any) {
+        // got error
+        toastShadcn({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description:
+            "An error has occurred while removing animal from activity: \n" +
+            error.message,
+        });
+      }
+    };
+    removeItemApi();
+  };
+
+  const removeItemDialogFooter = (
+    <React.Fragment>
+      <Button onClick={hideRemoveItemDialog}>
+        <HiX className="mr-2" />
+        No
+      </Button>
+      <Button variant={"destructive"} onClick={removeItem}>
+        <HiCheck className="mr-2" />
+        Yes
+      </Button>
+    </React.Fragment>
+  );
+  ////
+  const itemActionBodyTemplate = (item: EnrichmentItem) => {
+    return (
+      <React.Fragment>
+        <div className="mx-auto">
+          <Button
+            variant={"destructive"}
+            className="mr-2"
+            onClick={() => confirmRemoveItem(item)}
+          >
+            <HiTrash className="mx-auto" />
+          </Button>
+        </div>
+      </React.Fragment>
+    );
+  };
 
   return (
     <div>
@@ -371,6 +661,135 @@ function CreatePublicZooEventForm() {
             {!apiJson.loading ? <div>Submit</div> : <div>Loading</div>}
           </Button>
         </Form.Submit>
+
+
+        <div className="flex w-full gap-20">
+          <div className="w-full">
+            <div className="mb-2 text-xl font-medium">
+              Involved Animal(s):
+            </div>
+            <div className="flex justify-between">
+              <InputText
+                type="search"
+                placeholder="Search..."
+                onInput={(e) => {
+                  const target = e.target as HTMLInputElement;
+                  setInvolvedAnimalGlobalFilter(target.value);
+                }}
+                className="mb-2 h-full w-60"
+              />
+              {(employee.superAdmin ||
+                employee.planningStaff?.plannerType == "CURATOR") && (
+                  <Button
+                    onClick={() =>
+                      navigate(
+                        `/animal/assignanimalstoactivity/${curAnimalActivity.animalActivityId}`
+                      )
+                    }
+                    type="button"
+                    className="h-12 w-60"
+                  >
+                    Assign Animal(s)
+                  </Button>
+                )}
+            </div>
+            <DataTable
+              value={involvedAnimalList}
+              scrollable
+              scrollHeight="100%"
+              // selection={selectedAnimalToBecomeParent!}
+              selectionMode="single"
+              globalFilter={involvedAnimalGlobalFiler}
+              // onSelectionChange={(e) =>
+              //   setSelectedAnimalToBecomeParent(e.value)
+              // }
+              style={{ height: "50vh" }}
+              dataKey="animalCode"
+              className="h-1/2 overflow-hidden rounded border border-graydark/30"
+            >
+              <Column
+                field="imageUrl"
+                body={animalImageBodyTemplate}
+                style={{ minWidth: "3rem" }}
+              ></Column>
+              <Column
+                field="animalCode"
+                header="Code"
+                sortable
+                style={{ minWidth: "7rem" }}
+              ></Column>
+              <Column
+                field="houseName"
+                header="House Name"
+                sortable
+                style={{ minWidth: "5rem" }}
+              ></Column>
+              {(employee.superAdmin ||
+                employee.planningStaff?.plannerType == "CURATOR") && (
+                  <Column
+                    body={animalActionBodyTemplate}
+                    header="Actions"
+                    exportable={false}
+                    style={{ minWidth: "3rem" }}
+                  ></Column>
+                )}
+            </DataTable>
+            <Dialog
+              visible={removeAnimalDialog}
+              style={{ width: "32rem" }}
+              breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+              header="Confirm"
+              modal
+              footer={removeAnimalDialogFooter}
+              onHide={hideRemoveAnimalDialog}
+            >
+              <div className="confirmation-content">
+                <i
+                  className="pi pi-exclamation-triangle mr-3"
+                  style={{ fontSize: "2rem" }}
+                />
+                {selectedAnimal && (
+                  <span>
+                    Are you sure you want to remove{" "}
+                    {selectedAnimal.houseName} from the current
+                    activity? ?
+                  </span>
+                )}
+              </div>
+            </Dialog>
+          </div>
+          <div className="w-full">
+            <div className="mb-2 text-xl font-medium">
+              Item(s) to be used:
+            </div>
+            <div className="flex justify-between">
+              <InputText
+                type="search"
+                placeholder="Search..."
+                onInput={(e) => {
+                  const target = e.target as HTMLInputElement;
+                  setInvolvedItemGlobalFilter(target.value);
+                }}
+                className="mb-2 h-min w-60"
+              />
+              {(employee.superAdmin ||
+                employee.planningStaff?.plannerType == "CURATOR") && (
+                  <Button
+                    onClick={() =>
+                      navigate(
+                        `/animal/assignitemstoactivity/${curAnimalActivity.animalActivityId}`
+                      )
+                    }
+                    type="button"
+                    className="h-12 w-60"
+                  >
+                    Assign Item(s)
+                  </Button>
+                )}
+            </div>
+          </div>
+        </div>
+        <AllEmployeesDatatable></AllEmployeesDatatable>
       </Form.Root>
     </div>
   );
