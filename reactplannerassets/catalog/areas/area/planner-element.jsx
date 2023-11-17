9cc81f72078daade963427f1d9f820f55de4d614
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2023-11-17 19:18:46 +0800

    Fixed enclosure design areas import

--- reactplannerassets/catalog/areas/area/planner-element.jsx
+++ reactplannerassets/catalog/areas/area/planner-element.jsx
@@ -7,88 +7,94 @@ let info = {
   image: "",
 };
 
+import parquetTexture from "./textures/parquet.jpg";
+import tile1Texture from "./textures/tile1.jpg";
+import ceramicTileTexture from "./textures/ceramic-tile.jpg";
+import strandPorcelaneTexture from "./textures/strand-porcelain.jpg";
+import grassTexture from "./textures/grass.jpg";
+
 let textures = {
   parquet: {
     name: "Parquet",
-    uri: require("./textures/parquet.jpg"),
+    uri: parquetTexture,
     lengthRepeatScale: 0.004,
     heightRepeatScale: 0.004,
   },
   tile1: {
     name: "Tile1",
-    uri: require("./textures/tile1.jpg"),
+    uri: tile1Texture,
     lengthRepeatScale: 0.01,
     heightRepeatScale: 0.01,
   },
   ceramic: {
     name: "Ceramic Tile",
-    uri: require("./textures/ceramic-tile.jpg"),
+    uri: ceramicTileTexture,
     lengthRepeatScale: 0.02,
     heightRepeatScale: 0.02,
   },
   strand_porcelain: {
     name: "Strand Porcelain Tile",
-    uri: require("./textures/strand-porcelain.jpg"),
+    uri: strandPorcelaneTexture,
     lengthRepeatScale: 0.02,
     heightRepeatScale: 0.02,
   },
   grass: {
     name: "Grass",
-    uri: require("./textures/grass.jpg"),
+    uri: grassTexture,
     lengthRepeatScale: 0.01,
     heightRepeatScale: 0.01,
   },
 };
 // let textures = {};
 
-Promise.all([
-  import("./textures/parquet.jpg").then((uri) => {
-    textures.parquet = {
-      name: "Parquet",
-      uri: uri.default,
-      lengthRepeatScale: 0.004,
-      heightRepeatScale: 0.004,
-    };
-  }),
-  import("./textures/tile1.jpg").then((uri) => {
-    textures.tile1 = {
-      name: "Tile1",
-      uri: uri.default,
-      lengthRepeatScale: 0.01,
-      heightRepeatScale: 0.01,
-    };
-  }),
-  import("./textures/ceramic-tile.jpg").then((uri) => {
-    textures.ceramic = {
-      name: "Ceramic Tile",
-      uri: uri.default,
-      lengthRepeatScale: 0.02,
-      heightRepeatScale: 0.02,
-    };
-  }),
-  import("./textures/strand-porcelain.jpg").then((uri) => {
-    textures.strand_porcelain = {
-      name: "Strand Porcelain Tile",
-      uri: uri.default,
-      lengthRepeatScale: 0.02,
-      heightRepeatScale: 0.02,
-    };
-  }),
-  import("./textures/grass.jpg").then((uri) => {
-    textures.grass = {
-      name: "Grass",
-      uri: uri.default,
-      lengthRepeatScale: 0.01,
-      heightRepeatScale: 0.01,
-    };
-  }),
-])
-  .then(() => {
-    // All textures have been loaded
-    console.log("All textures loaded:", textures);
-  })
-  .catch((error) => {
-    console.error("Error loading textures:", error);
-  });
+// Promise.all([
+//   import("./textures/parquet.jpg").then((uri) => {
+//     textures.parquet = {
+//       name: "Parquet",
+//       uri: uri.default,
+//       lengthRepeatScale: 0.004,
+//       heightRepeatScale: 0.004,
+//     };
+//   }),
+//   import("./textures/tile1.jpg").then((uri) => {
+//     textures.tile1 = {
+//       name: "Tile1",
+//       uri: uri.default,
+//       lengthRepeatScale: 0.01,
+//       heightRepeatScale: 0.01,
+//     };
+//   }),
+//   import("./textures/ceramic-tile.jpg").then((uri) => {
+//     textures.ceramic = {
+//       name: "Ceramic Tile",
+//       uri: uri.default,
+//       lengthRepeatScale: 0.02,
+//       heightRepeatScale: 0.02,
+//     };
+//   }),
+//   import("./textures/strand-porcelain.jpg").then((uri) => {
+//     textures.strand_porcelain = {
+//       name: "Strand Porcelain Tile",
+//       uri: uri.default,
+//       lengthRepeatScale: 0.02,
+//       heightRepeatScale: 0.02,
+//     };
+//   }),
+//   import("./textures/grass.jpg").then((uri) => {
+//     textures.grass = {
+//       name: "Grass",
+//       uri: uri.default,
+//       lengthRepeatScale: 0.01,
+//       heightRepeatScale: 0.01,
+//     };
+//   }),
+// ])
+//   .then(() => {
+//     // All textures have been loaded
+//     console.log("All textures loaded:", textures);
+//   })
+//   .catch((error) => {
+//     console.error("Error loading textures:", error);
+//   });
 
 export default ElementsFactories.AreaFactory("area", info, textures);
